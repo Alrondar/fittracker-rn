ef6f1a383eb502d50058234ce120542d2833c6d9
Alrondar <alrondarr@gmail.com>
2026-08-13 14:40:47 +0300

    refactor: route exercise service through normalized reference data

---
+++ src/services/exercisesService.ts
@@ -0,0 +1,16 @@
+export {
+  getExercises,
+  getFilterOptions,
+  getExerciseById,
+  getExercisesByIds,
+  getExerciseRecords,
+} from './exercisesServiceNormalized';
+export type {
+  ExerciseListItem,
+  ExerciseSortBy,
+  ExerciseFilters,
+  ExerciseFilterOptions,
+  FilterOption,
+  ExerciseDetail,
+  ExerciseRecords,
+} from './exercisesServiceNormalized';
